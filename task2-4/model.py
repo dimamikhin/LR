0a258efff70dcd50518647fb1cda4ab13ef1accb
Дмитрий Михин <dimamikhin200184@yandex.ru>
2024-06-04 21:05:06 +0300

    upd

--- task2-4/model.py
+++ task2-4/model.py
@@ -1,72 +1,277 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score
+
 from __future__ import annotations
-from collections.abc import Iterator
 import datetime
-from typing import Optional, Union, Iterable
+from typing import (
+    Optional,
+    Union,
+    Iterable
+)
+
+import weakref
 
 
-class Sample:
+class Client:
+
     def __init__(
         self,
-        sepal_length: float,
-        sepal_width: float,
-        petal_length: float,
-        petal_width: float,
-        species: Optional[str] = None,
+        seniority: int,
+        home: int,
+        age: int,
+        marital: int,
+        records: int,
+        expenses: int,
+        assets: int,
+        amount: int,
+        price: int
     ) -> None:
-        self.sepal_length = sepal_length
-        self.sepal_width = sepal_width
-        self.petal_length = petal_length
-        self.petal_width = petal_width
-        self.species = species
-        self.classification: Optional[str] = None
+        self.seniority = seniority
+        self.home = home
+        self.age = age
+        self.marital = marital
+        self.records = records
+        self.expenses = expenses
+        self.assets = assets
+        self.amount = amount
+        self.price = price
 
     def __repr__(self) -> str:
-        if self.species is None:
-            known_unknown = "UnknownSample"
-        else:
-            known_unknown = "KnownSample"
-        if self.classification is None:
-            classification = ""
-        else:
-          classification = f", classification={self.classification!r}"
         return (
-            f"{known_unknown}("
-            f"sepal_length={self.sepal_length}, "
-            f"sepal_width={self.sepal_width}, "
-            f"petal_length={self.petal_length}, "
-            f"petal_width={self.petal_width}, "
-            f"species={self.species!r}"
-            f"{classification}"
+            f"{self.__class__.__name__}("
+            f"seniority={self.seniority}"
+            f"home={self.home}"
+            f"age={self.age}"
+            f"marital={self.marital}"
+            f"records={self.records}"
+            f"expenses={self.assets}"
+            f"amount={self.amount}"
+            f"price={self.price}"
             f")"
         )
 
-    def classify(self, classification: str) -> None:
+class KnownClient(Client):
+    def __init__(self,
+        status: int,
+        seniority: int,
+        home: int,
+        age: int,
+        marital: int,
+        records: int,
+        expenses: int,
+        assets: int,
+        amount: int,
+        price: int
+    ) -> None:
+        super().__init__(
+            seniority=seniority,
+            home=home,
+            age=age,
+            marital=marital,
+            records=records,
+            expenses=expenses,
+            assets=assets,
+            amount=amount,
+            price=price
+        )
+        self.status = status
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}("
+            f"seniority={self.seniority},"
+            f"home={self.home},"
+            f"age={self.age},"
+            f"marital={self.marital},"
+            f"records={self.records},"
+            f"expenses={self.assets},"
+            f"amount={self.amount},"
+            f"price={self.price},"
+            f"status={self.species!r},"
+            f")"
+        )
+
+
+class TrainingKnownClient(KnownClient):
+    pass
+
+class TestingKnownClient(KnownClient):
+    def __init__(self,
+        status: int,
+        seniority: int,
+        home: int,
+        age: int,
+        marital: int,
+        records: int,
+        expenses: int,
+        assets: int,
+        amount: int,
+        price: int,
+        classification: Optional[str] = None
+    ) -> None:
+        super().__init__(
+            status,
+            seniority,
+            home,
+            age,
+            marital,
+            records,
+            expenses,
+            assets,
+            amount,
+            price
+        )
         self.classification = classification
 
-    def matches(self) -> bool:
-        return self.species == self.classification
+    def mathces(self) -> bool:
+        self.species = self.classification
 
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}("
+            f"seniority={self.seniority},"
+            f"home={self.home},"
+            f"age={self.age},"
+            f"marital={self.marital},"
+            f"records={self.records},"
+            f"expenses={self.assets},"
+            f"amount={self.amount},"
+            f"price={self.price},"
+            f"status={self.species!r},"
+            f"classification={self.classification!r}"
+            f")"
+        )
+
+
+class UnknownClient(Client):
+    pass
+
+
+class ClassifiedClient(Client):
+    def __init__(self, classification: str, client: UnknownClient) -> None:
+        super().__init__(
+            seniority=client.seniority,
+            home=client.home,
+            age=client.age,
+            marital=client.marital,
+            records=client.records,
+            expenses=client.expenses,
+            assets=client.assets,
+            amount=client.amount,
+            price=client.price
+        )
+        self.classification = classification
+
+    def __repr__(self) -> str:
+        return (
+            f"{self.__class__.__name__}("
+            f"seniority={self.seniority},"
+            f"home={self.home},"
+            f"age={self.age},"
+            f"marital={self.marital},"
+            f"records={self.records},"
+            f"expenses={self.assets},"
+            f"amount={self.amount},"
+            f"price={self.price},"
+            f"classification={self.classification!r}"
+            f")"
+        )
 
 class Hyperparameter:
-    """Значение гиперпараметра и общее качество классификации."""
-
-    def __init__(self, k: int, training: "TrainingData") -> None:
-        self.k = k
-        self.data: TrainingData = training
+    def __init__(self, max_depth: int, min_sample_size: int, training: "TrainingData") -> None:
+        self.max_depth = max_depth
+        self.min_leaf_size = min_sample_size
+        self.data: weakref.ReferenceType["TrainingData"] = weakref.ref(training)
         self.quality: float
 
     def test(self) -> None:
-        """Выполняет проверку на тестовом наборе данных"""
-        pass_count, fail_count = 0, 0
-        for sample in self.data.testing:
-            sample.classification = self.classify(sample)
-            if sample.matches():
-                pass_count += 1
+        trainingData: Optional["TrainingData"] = self.data()
+        if not trainingData:
+            raise RuntimeError("Broken Waek Reference")
+        test_data = trainingData.testing
+        x_test = TrainingData.get_list_clients(test_data)
+        y_test = TrainingData.get_statuses_clients(test_data)
+        y_predict = self.classify_list(x_test)
+        self.quality = roc_auc_score(y_test, y_predict)
+        for i in range(len(y_predict)):
+            test_data[i].classification = y_predict[i]
+
+    def classify_list(self, clients: list[Union[UnknownClient, TestingKnownClient]]) -> list:
+        training_data = self.data
+        if not training_data:
+            raise RuntimeError("No training object")
+        x_predict = TrainingData.get_list_clients(clients)
+        x_train = TrainingData.get_list_clients(training_data)
+        y_train = TrainingData.get_statuses_clients(training_data)
+
+        classifier = DecisionTreeClassifier()
+        classifier = classifier.fit(x_train, y_train)
+        return classifier.predict(x_predict).tolist()
+
+
+
+
+class TrainingData:
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.uploaded: datetime.datetime
+        self.tested: datetime.datetime
+        self.training: list[TrainingKnownClient] = []
+        self.testing: list[TestingKnownClient] = []
+        self.tuning: list[Hyperparameter] = []
+
+    def load(self, raw_data_soruce: Iterable[dict[str, str]]) -> None:
+
+        for n, row in enumerate(raw_data_soruce):
+            client = Client(
+                seniority = int(row["seniority"]),
+                home = int(row["home"]),
+                age = int(row["age"]),
+                marital = int(row["marital"]),
+                records = int(row["records"]),
+                expenses = int(row["expenses"]),
+                assets = int(row["assets"]),
+                amount = int(row["amount"]),
+                price = int(row["price"]),
+                status = row["status"]
+            )
+            if n % 5 == 0:
+                self.testing.append(client)
             else:
-                fail_count += 1
-        self.quality = pass_count / (pass_count + fail_count)
+                self.training.append(client)
+        self.uploaded = datetime.date.now(tz=datetime.timezone.utc)
 
-    def classify(self, sample: Sample) -> str:
-        """TODO: алгоритм k-NN"""
-        return ""
+    def test(self, parameter: Hyperparameter) -> None:
+      
 
+        parameter.test()
+        self.tuning.append(parameter)
+        self.tested = datetime.datetime.now(tz=datetime.timezone.utc)
+
+
+    def classify(self, parameter: Hyperparameter, client: Client) -> Client:
+
+        classification = parameter.classify(client)
+        client.classify(classification)
+        return client
+
+    @staticmethod
+    def get_list_clients(clients: list[Client]) -> list:
+        return [
+            [
+                client.seniority,
+                client.home,
+                client.age,
+                client.marital,
+                client.records,
+                client.expenses,
+                client.assets,
+                client.amount,
+                client.price
+            ]
+            for client in clients
+        ]
+
+    @staticmethod
+    def get_statuses_clients(clients: list[KnownClient]) -> list:
+        return [client.status for client in clients]
